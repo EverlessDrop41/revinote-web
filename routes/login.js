d188be8cd1f94deca095f5da5725cc38db80bb4b
Emily Peregrine <tomfperegrine@googlemail.com>
2015-10-10 18:40:37 +0100

    Added signup and modified form

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,21 @@
 module.exports = function (app) {
 	app.get('/login', function (req, res) {
-		res.render('login');
+		res.render('login', {signup: false});
 	});
 
 	app.post('/login', function (req, res) {
 		console.log(req.body.email);
 		console.log(req.body.password);
-		res.render('login');
+		res.render('login', {signup: false});
+	});
+
+	app.get('/sign_up', function (req, res) {
+		res.render('login', {signup: true});
+	});
+
+	app.post('/sign_up', function (req, res) {
+		console.log(req.body.email);
+		console.log(req.body.password);
+		res.redirect('login');
 	});
 };
