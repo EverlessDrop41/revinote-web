b0a4be74f4a8c34a0b486ac3f02d125ff58bdcd1
Emily Peregrine <tomfperegrine@googlemail.com>
2015-10-14 22:54:52 +0100

    Can now retreive list of subjects from firebase

--- public/js/subjects-page.js
+++ public/js/subjects-page.js
@@ -1,1 +1,30 @@
-console.log(FireRef, uid);
+//Emily Peregrine
+jQuery(document).ready(function ($) {
+	if (uid && FireRef) {
+		userBase = FireRef.child(uid);
+		subjectBase = userBase.child("subjects");
+		
+		subjectBase.on("value", function(snap) {
+			snap.forEach(function (subject) {
+				console.log(subject.val());
+			});
+		}, function (err) {
+			console.error(err.code);
+		});
+
+		//subjectBase.push({name: "English",description: "Words 'n Stuff"});
+	} else {
+		console.error("Uid and/or FireRef not assigned");
+	}
+});
+
+function Subject (name, description){
+	this.name = name;
+	this.description = description;
+	this.getUrl = function () {
+		return "/posts/subjects/" + name;
+	}
+	this.getData = function () {
+		return {name: name, description: description};
+	}
+}
