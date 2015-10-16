b0687ce37cd1c501c808b113bcf8ef999b6b40bd
Emily Peregrine <tomfperegrine@googlemail.com>
2015-10-16 16:10:38 +0100

    Added basic template for post viewing

--- routes/notes.js
+++ routes/notes.js
@@ -2,6 +2,15 @@ module.exports = function (app) {
 	app.get('/subjects/:subject', must_be_logged_in, function (req, res) {
 		res.render('posts/notes-list.swig', { user: req.session.user, subject: req.params.subject.toLowerCase() });
 	});
+
+	app.get('/subjects/:subject/:noteid', must_be_logged_in, function (req, res) {
+		res.render('posts/note.swig', { 
+			user: req.session.user, 
+			subject: req.params.subject.toLowerCase(), 
+			noteId: req.params.noteid,
+			Title: req.params.noteid.toString()
+		});
+	});
 };
 
 function must_be_logged_in(req, res, next) {
