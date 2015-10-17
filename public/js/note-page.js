cdc1afb8211559722028a76318555dbbdcbfaf33
Emily Peregrine <tomfperegrine@googlemail.com>
2015-10-17 17:37:09 +0100

    Can now view notes

--- public/js/note-page.js
+++ public/js/note-page.js
@@ -1,1 +1,36 @@
 //Note Page
+jQuery(document).ready(function ($) {
+	TitleBox = $("#Title");
+	ContentBox = $("#Content");
+
+
+	LoadingBox = $("#loading");
+
+	if (hasRequiredData && FireRef) {
+		TitleBox.hide();
+		ContentBox.hide();
+
+		userBase = FireRef.child(uid);
+		notesBase = userBase.child("notes");
+		subjectBase = notesBase.child(subject);
+		note = subjectBase.child(noteId);
+		
+		note.on("value", function(snap) {
+			LoadingBox.show();
+			TitleBox.hide();
+			ContentBox.hide();
+
+			var data = snap.val();
+			TitleBox.text(data.title);
+			ContentBox.html(data.content);
+
+			LoadingBox.hide();
+			TitleBox.show();
+			ContentBox.show();
+		}, function (err) {
+			console.error(err.code);
+		});
+	} else {
+		console.error("Uid and/or FireRef and/or Subject and/or Note Id not assigned");
+	}
+});
