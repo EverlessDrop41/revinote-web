b06bb8541f0e0a79c521f11b038c8122c91da2c3
Emily Peregrine <tomfperegrine@googlemail.com>
2015-10-18 16:22:27 +0100

    Setup for heroku

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ require('./routes/notes.js')(app);
 require('./routes/post-list.js')(app);
 
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
