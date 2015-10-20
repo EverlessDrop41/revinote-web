352a2b259954bd2f6221382c67c9cfad8c0ebe66
Emily Peregrine <tomfperegrine@googlemail.com>
2015-10-20 16:11:37 +0100

    Now have post previewing

--- public/js/note-editor.js
+++ public/js/note-editor.js
@@ -1,6 +1,30 @@
-//Note Editor
-var converter = new Markdown.Converter();
-var editor = new Markdown.Editor(converter);
+jQuery(document).ready(function ($) {
+	//Note Editor
+	var converter = new Markdown.Converter();
+	var editor = new Markdown.Editor(converter);
 
-editor.getConverter();
-editor.run();
+	editor.getConverter();
+	editor.run();
+
+	var ToggleBtn = $("#ToggleVisibility");
+	var ToggleIcon = $(ToggleBtn.attr('data-icon'));
+
+	var eyeOpen = 'fa-eye';
+	var eyeClosed = eyeOpen + '-slash';
+
+	var wmd_preview = $("#wmd-preview");
+
+	ToggleBtn.click(function () {
+		wmd_preview.slideToggle();
+
+		if (ToggleIcon.hasClass(eyeOpen)) {
+			ToggleIcon.removeClass(eyeOpen);
+			ToggleIcon.addClass(eyeClosed);
+		} else {
+			ToggleIcon.removeClass(eyeClosed);
+			ToggleIcon.addClass(eyeOpen);
+		}
+	});
+
+	ToggleBtn.click();
+});
