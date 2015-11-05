da650d4c46dc480395ed09abf7e819e6d31a5566
emilyperegrine <tomfperegrine@googlemail.com>
2015-11-05 17:23:22 +0000

    Created Word Class

--- public/js/widgets/dictionary-widget.js
+++ public/js/widgets/dictionary-widget.js
@@ -1,4 +1,32 @@
 jQuery(document).ready(function ($) {
+
+	function Word (word, results, syllables){
+		this.word = word;
+		this.results = results;
+		this.syllables = syllables;
+	};
+
+	Word.prototype.resultsHtml = function() {
+		rerStr = "";
+
+		arrayLength = this.results.length;
+		for (var i = 0; i < arrayLength; i++) {
+		  res = this.results[i];
+		  rerStr += "<em>" + res.partOfSpeech + "</em>";
+		  rerStr += "<p>" + res.definition + "</p>";
+		  if (i != arrayLength - 1) {
+		  	rerStr += "<hr>";
+		  }
+		}
+
+		return rerStr;
+	};
+
+	Word.prototype.generateHtml = function() {
+		return "<h2>" + this.word + "</h2>" + this.resultsHtml();
+	};
+
+
 	dw = $("#DictionaryWidget");
 
 	$("#DictionaryWidget").dialog();
@@ -19,7 +47,8 @@ jQuery(document).ready(function ($) {
 	  	xhr.setRequestHeader("X-Mashape-Key", "8eVWqtLfcGmsh6SVYIdtEi3RFU0wp1eHyfFjsn55YAFaazgReV");
 	  },
 	  success: function (data) {
-			console.log(data);
+	  	w = new Word(data.word, data.results, data.syllables);
+			console.log(w.generateHtml());
 		}
 	});
 });
