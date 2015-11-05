e8f52681214e703e434779270cf4333ab6748129
emilyperegrine <tomfperegrine@googlemail.com>
2015-11-05 14:51:01 +0000

    Fixed browser widget

--- public/js/widgets/browser-widget.js
+++ public/js/widgets/browser-widget.js
@@ -1,13 +1,15 @@
-/*jQuery(document).ready(function ($) {
+jQuery(document).ready(function ($) {
 	$("#BrowserWidget").dialog({
 		resize: function (event, ui) {
 			frame = $("#BrowserWidgetIFrame");
 			heightPadding = frame.outerHeight() - frame.innerHeight();
 			frame.height($("#BrowserWidget").height() - heightPadding);
 
-			frame.width($("#BrowserWidget").width() + 30);
-
-			$("#BrowserWidget").css('margin', "0px !important");
+			frame.width($("#BrowserWidget").width());
 		}
 	});
-});*/
+
+	$("#BrowserToggle").click(function () {
+		//Toggle browser visbility
+	});
+});
