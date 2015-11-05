d62bf318850b86c7054e8cc032895c195c635257
emilyperegrine <tomfperegrine@googlemail.com>
2015-11-05 14:16:18 +0000

    Tried to make a basic browser widget

---
+++ public/js/widgets/browser-widget.js
@@ -0,0 +1,13 @@
+/*jQuery(document).ready(function ($) {
+	$("#BrowserWidget").dialog({
+		resize: function (event, ui) {
+			frame = $("#BrowserWidgetIFrame");
+			heightPadding = frame.outerHeight() - frame.innerHeight();
+			frame.height($("#BrowserWidget").height() - heightPadding);
+
+			frame.width($("#BrowserWidget").width() + 30);
+
+			$("#BrowserWidget").css('margin', "0px !important");
+		}
+	});
+});*/
