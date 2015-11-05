1ae86d276d39cb4981eac90ee3937a291b2a865e
emilyperegrine <tomfperegrine@googlemail.com>
2015-11-05 17:00:35 +0000

    Added test request for dictionary api

--- public/js/widgets/dictionary-widget.js
+++ public/js/widgets/dictionary-widget.js
@@ -12,4 +12,14 @@ jQuery(document).ready(function ($) {
 			dw.dialog('open');
 		}
 	});
+
+	$.ajax({
+		url : "https://wordsapiv1.p.mashape.com/words/bump", 
+		beforeSend: function( xhr ) {
+	  	xhr.setRequestHeader("X-Mashape-Key", "8eVWqtLfcGmsh6SVYIdtEi3RFU0wp1eHyfFjsn55YAFaazgReV");
+	  },
+	  success: function (data) {
+			console.log(data);
+		}
+	});
 });
